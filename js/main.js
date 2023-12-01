46fcbcbcec5eb552e2e7181a19921fc36f56bd90
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-01 14:16:57 +0100

    Impostato Esercizio

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* 
+
+    Il programma dovrà chiedere all'utente:
+
+    1) il numero di chilometri che vuole percorrere il passeggeo;
+    2) l'età del passeggero;
+
+    Sulla base di queste informazioni dovrà calcolare:
+
+    3) il prezzo totale del viaggio --> secondo queste regole:
+
+    a) il prezzo del biglietto è definito in base ai km (0.21 € al km);
+    b) va applicato uno sconto del 20% per i minorenni;
+    c) va applicato uno sconto del 40% per gli over 65.
+
+    4) L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+*/
+
